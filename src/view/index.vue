ebc0388be75017666731bdc4d218aa2cf03fe42d
方势 <fangshi@xiaomi.com>
2018-07-30 11:44:54 +0800

    init

---
+++ src/view/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <transition name="fade">
+      <div v-if="showState">
+        <IgwHeader />
+      </div>
+    </transition>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import IgwHeader from '../view/IgwHeader'
+
+export default {
+  name: 'Index',
+  components: {
+    IgwHeader
+  },
+  data () {
+    return {
+      showState: false
+    }
+  },
+
+  created () {
+    setTimeout(() => {
+      this.showState = true
+    }, 0)
+  }
+}
+</script>
