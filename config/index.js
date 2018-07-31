1e490d960013ef19a920498c194857a61ac506ce
方势 <fangshi@xiaomi.com>
2018-07-31 15:17:40 +0800

    😆

--- config/index.js
+++ config/index.js
@@ -45,10 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
-
+    // index: path.resolve(__dirname, '../dist/index.html'),
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+
+    index: path.resolve(__dirname, '../../blog/dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../blog/dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
